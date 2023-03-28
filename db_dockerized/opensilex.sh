6a3e65b502604125bc74911f54ffc5ce4b5264ab
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2023-03-28 09:31:11 +0200

    Update branch

--- db_dockerized/opensilex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-
-java -jar $SCRIPT_DIR/opensilex/opensilex.jar --BASE_DIRECTORY=$SCRIPT_DIR/opensilex --CONFIG_FILE=config/opensilex.yml "$@"
-
- 
